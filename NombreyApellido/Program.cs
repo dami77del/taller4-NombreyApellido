968081229b3972dff0d668765b15b24aeb4ec210
Damian <damian.deligdisch@davinci.edu.ar>
2018-05-10 17:35:34 -0300

    Sincro

---
+++ NombreyApellido/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NombreyApellido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String nombre;
+            String apellido;
+
+            Console.WriteLine("Escriba su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escriba su apellido");
+            apellido = Console.ReadLine();
+            Console.WriteLine("Nombre"+ nombre + " Apellido" + apellido);
+        }
+    }
+}
